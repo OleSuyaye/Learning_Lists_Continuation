58add7054de15fb6d036aa9b310d41cb47ac1ae4
OleSuyaye <Olesuyaye@yahoo.com>
2025-12-11 15:35:28 +0300

    functions, average, and sorting in lists!

--- Lists.py
+++ Lists.py
@@ -33,3 +33,27 @@ def average (x):
 print(f"The average marks of 4W, 4E, and 4S are as follows: 4W: {average(four_west_marks)},"
       f" 4E: {average(four_east_marks)},"
       f" 4S: {average(four_south_marks)}.")
+
+#Class with the highest average
+four_west_average = average(four_west_marks)
+four_east_average = average(four_east_marks)
+four_south_average = average(four_south_marks)
+
+if four_west_average > four_east_average and four_west_average > four_south_average:
+    print(f"4W has the highest average of {four_west_average}")
+elif four_east_average > four_west_average and four_east_average > four_south_average:
+    print(f"4E has the highest average of {four_east_average}")
+else:
+    print(f"4S has the highest average of {four_south_average}")
+
+#Sorting the marks
+def marks_sorting(x):
+    sorted_marks = sorted(x)
+    return sorted_marks
+
+print(marks_sorting(four_west_marks))
+print(marks_sorting(four_east_marks))
+print(marks_sorting(four_south_marks))
+
+four_west_marks.sort()
+print(four_west_marks)
